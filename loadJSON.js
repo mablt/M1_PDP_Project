f7bfa41e7eee4a7be185a0238bfc55fd9ee64ec1
marineadb <marineadb@gmail.com>
2020-04-13 14:07:56 +0200

    update load json

--- loadJSON.js
+++ loadJSON.js
@@ -1,9 +1,5 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
+"use strict";
+import * as fs from 'fs';
 
-var rawFile = new XMLHttpRequest();
-rawFile.open("GET", "./../testdata/ecoli/e_coli_glycolysis_gluconeogenesis.json", false);
-// const json = '{"result":true, "count":42}';
-const obj = JSON.parse(rawFile);
-
-console.log(obj.);
+export var json = JSON.parse(fs.readFileSync('./data.json').toString());
