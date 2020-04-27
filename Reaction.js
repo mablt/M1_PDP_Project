439af30d7f1b0d9703686631f3d9ff291a24b7a5
marineadb <marineadb@gmail.com>
2020-04-27 16:48:51 +0200

    Updated Reaction class

--- Reaction.js
+++ Reaction.js
@@ -11,5 +11,52 @@ export class Reaction extends Relation{
         this.enzyme = "";
 
     }
+    
+    // Getters
+    getLowerBound(){
+        return this.lowerBound;
+    }
+    getUpperBound(){
+        return this.upperBound;
+    }
+    getSubsystem(){
+        return this.substystem;
+    }
+    getNotes(){
+        return this.notes;
+    }
+    getAnnotation(){
+        return this.annotation;
+    }
+    getEnzyme(){
+        return this.enzyme;
+    }
+
+    //Setters
+    setLowerBound(lowerBound){
+        this.lowerBound = lowerBound;
+    }
+    setUpperBound(upperBound){
+        this.upperBound=upperBound;
+    }
+
+    setSubsystem(subsystem){
+        this.substystem = subsystem;
+    }
+
+    setNotes(notes){
+        this.notes = notes;
+    }
+
+    setAnnotation(annotation){
+        this.annotation = annotation;
+    }
+
+    addAnotation(DbName, annotation){
+        this.annotation.add(DbName,annotation);
+    }
+    setEnzyme(enzyme){
+        this.enzyme=enzyme;
+    }
 
 }
