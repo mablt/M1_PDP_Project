0beddb531df8643ea6d7b94b6d6ad42094e642e5
mablt <mathieu.bolteau1@gmail.com>
2020-04-27 16:26:08 +0200

    Graph class initialisation

--- Graph.js
+++ Graph.js
@@ -1,1 +1,20 @@
-ls
+import { Element } from Element.js;
+
+export class Graph{
+
+    constructor(){
+        elements = [];
+    }
+
+    addElement(element){
+        this.elements.push(element);
+    }
+
+    removeElement(id){
+        for(var i=0; i< this.elements.length; i++){
+            if (element.id === id) {
+                elements.splice(i, 1);
+            }
+        }
+    }
+}
