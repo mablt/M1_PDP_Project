c1644761de5e191a93c262fd70fe3358ce780fbb
mablt <mathieu.bolteau1@gmail.com>
2020-05-09 17:35:52 +0200

    Update

--- test/test3dForce.js
+++ test/test3dForce.js
@@ -1,5 +1,5 @@
 // Import
-import { parseJSON, create3dForceObject } from "./../src/utils.js";
+import { parseJSON, create3dForceObject, stringToJSON } from "./../src/utils.js";
 
 
 function displayGraph(object){
@@ -16,22 +16,45 @@ function displayGraph(object){
 }
 
 
-fetch('./dataTest.json')
-    .then (function(response){
-        return response.json();
-    })
-    .then(function(data){
-        console.log(data);
-        var pathwayCreatedByParseJSON = parseJSON(data);
+export function loadFileAsText(){
+    var fileToLoad = document.getElementById("files").files[0];
+    var fileReader = new FileReader();
+    fileReader.onload = function(fileLoadedEvent){
+        var textFromFileLoaded = fileLoadedEvent.target.result;
+        // Create the graph
+        jsonFileToGraph(textFromFileLoaded);
+    };
+    fileReader.readAsText(fileToLoad, "UTF-8");
+}
 
-        console.log("\n\n\n PATHWAY CREATED \n\n");
-        console.log(pathwayCreatedByParseJSON);
-        var object = create3dForceObject(pathwayCreatedByParseJSON);
-        console.log(object);
-        displayGraph(object);
+
+// Read and parse the JSON file to display the graph
+export function jsonFileToGraph(data){
+    var jsonObject = stringToJSON(data);
+    var pathwayCreatedByParseJSON = parseJSON(jsonObject);
+    var object = create3dForceObject(pathwayCreatedByParseJSON);
+    displayGraph(object);
+}
+
+
+// OLD METHOD
+
+// fetch('./dataTest.json')
+//     .then (function(response){
+//         return response.json();
+//     })
+//     .then(function(data){
+//         console.log(data);
+//         var pathwayCreatedByParseJSON = parseJSON(data);
+
+//         console.log("\n\n\n PATHWAY CREATED \n\n");
+//         console.log(pathwayCreatedByParseJSON);
+//         var object = create3dForceObject(pathwayCreatedByParseJSON);
+//         console.log(object);
+//         displayGraph(object);
         
-    })
-    .catch(function(err){
-        console.log("ERROR");
-    });
+//     })
+//     .catch(function(err){
+//         console.log("ERROR");
+//     });
 
