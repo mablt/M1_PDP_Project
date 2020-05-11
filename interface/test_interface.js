8f8e62ae134cdeb9d03bdb20347d182317392b4e
Solweig Hennechart <solweig.hennechart@etu.u-bordeaux.fr>
2020-05-11 09:52:51 +0000

    Update test_interface.js for upload/save

--- interface/test_interface.js
+++ interface/test_interface.js
@@ -5,10 +5,36 @@ function displayGraph(object){
     (document.getElementById('p3d-graph'))
     .nodeAutoColorBy('group')
       .linkOpacity(0.5)
-      .graphData(object);
+      .graphData(object)
+      .onNodeDragEnd(node => {
+          node.fx = node.x;
+          node.fy = node.y;
+          node.fz = node.z;
+        });
+}
+
+export function loadFileAsText(){
+  var fileToLoad = document.getElementById("files").files[0];
+  var fileReader = new FileReader();
+  fileReader.onload = function(fileLoadedEvent){
+      var textFromFileLoaded = fileLoadedEvent.target.result;
+      // Create the graph
+      jsonFileToGraph(textFromFileLoaded);
+  };
+  fileReader.readAsText(fileToLoad, "UTF-8");
 }
 
 
+// Read and parse the JSON file to display the graph
+export function jsonFileToGraph(data){
+  var jsonObject = stringToJSON(data);
+  var pathwayCreatedByParseJSON = parseJSON(jsonObject);
+  var object = create3dForceObject(pathwayCreatedByParseJSON);
+  displayGraph(object);
+}
+
+
+/*
 fetch('./ecoli_e_coli_core.json')
   .then (function(response){
       return response.json();
@@ -27,4 +53,4 @@ fetch('./ecoli_e_coli_core.json')
   .catch(function(err){
       console.log("ERROR");
   }
-  );
+  );*/
